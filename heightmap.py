9d877e93b5f8113fd73f01aa4881cbb83e950919
Cristhyan De Marchena <demarchenac@uninorte.edu.co>
2020-08-17 18:46:26 -0500

    added slide bar

--- heightmap.py
+++ heightmap.py
@@ -8,6 +8,8 @@ Created on Fri Aug 14 11:58:01 2020
 
 import vtk
 
+warp = vtk.vtkWarpScalar()
+
 def get_program_parameters():
     import argparse
     parser = argparse.ArgumentParser()
@@ -17,6 +19,42 @@ def get_program_parameters():
     
     return args.vti_file, args.texture_file
 
+def generate_slide_bar():
+    red_r = 224/255
+    red_g = 69/255
+    red_b = 85/255
+    green_r = 70/255
+    green_g = 224/255
+    green_b = 105/255
+    white = 242/255
+    
+    #build a slide bar
+    slide_bar = vtk.vtkSliderRepresentation2D()
+    
+    slide_bar.SetMinimumValue(-100.0)
+    slide_bar.SetMaximumValue(100.0)
+    slide_bar.SetTitleText("Scalar Value")
+    
+    slide_bar.GetSliderProperty().SetColor(red_r, red_g, red_b)
+    slide_bar.GetTitleProperty().SetColor(white, white, white)
+    slide_bar.GetLabelProperty().SetColor(red_r, red_g, red_b)
+    slide_bar.GetSelectedProperty().SetColor(green_r, green_g, green_b)
+    slide_bar.GetTubeProperty().SetColor(white, white, white)
+    slide_bar.GetCapProperty().SetColor(red_r, red_g, red_b)
+    
+    slide_bar.GetPoint1Coordinate().SetCoordinateSystemToDisplay()
+    slide_bar.GetPoint1Coordinate().SetValue(30,100)
+    
+    slide_bar.GetPoint2Coordinate().SetCoordinateSystemToDisplay()
+    slide_bar.GetPoint2Coordinate().SetValue(330,100)
+    return slide_bar
+
+def custom_callback(obj, event):
+    # print("interaction called")
+    value = int (obj.GetRepresentation().GetValue())
+    global warp
+    warp.SetScaleFactor(value)
+    warp.Update()
 
 def main():
     # Define stuff
@@ -38,7 +76,7 @@ def main():
     atext.InterpolateOn()
 
     # generate warp from vti.
-    warp = vtk.vtkWarpScalar()
+    global warp
     warp.SetInputConnection(reader.GetOutputPort())
     warp.SetScaleFactor(-75)
     
@@ -61,30 +99,39 @@ def main():
     #actor.SetTexture(atext)
     
     # Create renderer stuff
-    ren = vtk.vtkRenderer()
-    renWin = vtk.vtkRenderWindow()
-    renWin.AddRenderer(ren)
-    iren = vtk.vtkRenderWindowInteractor()
-    iren.SetRenderWindow(renWin)
+    renderer = vtk.vtkRenderer()
+    renderer_window = vtk.vtkRenderWindow()
+    renderer_window.AddRenderer(renderer)
+    renderer_window_interactor = vtk.vtkRenderWindowInteractor()
+    renderer_window_interactor.SetRenderWindow(renderer_window)
+    
+    # add slide bar   
+    slide_bar = generate_slide_bar()
+    slider_widget = vtk.vtkSliderWidget()
+    slider_widget.SetInteractor(renderer_window_interactor)
+    slider_widget.SetRepresentation(slide_bar)
+    slider_widget.AddObserver("InteractionEvent", custom_callback)
+    slider_widget.EnabledOn()
+
     
     # Add the actors to the renderer, set the background and size
-    ren.AddActor(actor)
-    ren.ResetCamera()
-    ren.GetActiveCamera().Azimuth(180)
-    ren.GetActiveCamera().Roll(180)
-    ren.GetActiveCamera().Yaw(0)
-    ren.GetActiveCamera().Elevation(0)
-    ren.SetBackground(0.1, 0.1, 0.1)
-    ren.ResetCameraClippingRange()
+    renderer.AddActor(actor)
+    renderer.ResetCamera()
+    renderer.GetActiveCamera().Azimuth(180)
+    renderer.GetActiveCamera().Roll(180)
+    renderer.GetActiveCamera().Yaw(0)
+    renderer.GetActiveCamera().Elevation(0)
+    renderer.SetBackground(0.1, 0.1, 0.1)
+    renderer.ResetCameraClippingRange()
     
-    renWin.SetSize(600, 400)
+    renderer_window.SetSize(1200, 800)
     
-    cam1 = ren.GetActiveCamera()
+    cam1 = renderer.GetActiveCamera()
     cam1.Zoom(1.5)
     
-    iren.Initialize()
-    renWin.Render()
-    iren.Start()
+    renderer_window_interactor.Initialize()
+    renderer_window.Render()
+    renderer_window_interactor.Start()
 
 if __name__ == '__main__':
     main()
