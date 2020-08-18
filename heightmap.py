82992314a49b1200378fccd8bdff08651346ea30
Cristhyan De Marchena <demarchenac@uninorte.edu.co>
2020-08-17 20:16:56 -0500

    Finished!

--- heightmap.py
+++ heightmap.py
@@ -5,7 +5,6 @@ Created on Fri Aug 14 11:58:01 2020
 @author: John Corro
 @author: Cristhyan De Marchena
 """
-
 import vtk
 
 warp = vtk.vtkWarpScalar()
@@ -13,13 +12,67 @@ warp = vtk.vtkWarpScalar()
 def get_program_parameters():
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('vti_file', nargs='?', default=None, help='vti file')
-    parser.add_argument('texture_file', nargs='?', default=None, help='jpg file')
+    parser.add_argument('data_file', nargs='?', default=None, help='data file')
+    parser.add_argument('texture_file', nargs='?', default=None, help='texture file')
     args = parser.parse_args()
     
-    return args.vti_file, args.texture_file
+    return args.data_file, args.texture_file
+
+def read_file(file_name):
+    import os
+    if(file_name):
+        path, extension = os.path.splitext(file_name)
+        extension = extension.lower()
+        if extension == ".vti":
+            reader = vtk.vtkXMLImageDataReader()
+            reader.SetFileName(file_name)
+        elif extension == ".vtp":
+            reader = vtk.vtkXMLPolyDataReader()
+            reader.SetFileName(file_name)
+        elif extension == ".jpg":
+            readerFactory = vtk.vtkImageReader2Factory()
+            img_file = readerFactory.CreateImageReader2(file_name)
+            img_file.SetFileName(file_name)
+            img_file.Update()
+            reader = img_file
+        else:
+            # the file provided doesn't match the accepted extenstions
+            reader = None
+    else:
+        reader = None
+    return reader
+
+def generate_texture(texture_file):
+    texture_file = read_file(texture_file)
+    if(texture_file):
+        texture = vtk.vtkTexture()
+        texture.SetInputConnection(texture_file.GetOutputPort())
+        texture.InterpolateOn()
+    else: 
+        texture = None
+    return texture
+    
+
+def generate_actor(data, texture):
+    # Warp the input data.
+    global warp
+    warp.SetInputConnection(data.GetOutputPort())
+    warp.SetScaleFactor(0)
+    
+    # Add mapper.
+    mapper = vtk.vtkDataSetMapper()
+    mapper.SetInputConnection(warp.GetOutputPort())
+    mapper.SetScalarRange(0, 255)
+    mapper.ScalarVisibilityOff()
+    
+    # Generate actor from mapper.
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+    actor.SetTexture(texture)
+    return actor
 
 def generate_slide_bar():
+    # Slidebar colors
     red_r = 224/255
     red_g = 69/255
     red_b = 85/255
@@ -28,14 +81,16 @@ def generate_slide_bar():
     green_b = 105/255
     white = 242/255
     
-    #build a slide bar
+    # Create Slidebar
     slide_bar = vtk.vtkSliderRepresentation2D()
     
+    # Set range and title.
     slide_bar.SetMinimumValue(-100.0)
     slide_bar.SetMaximumValue(100.0)
-    slide_bar.SetValue(-75)
+    slide_bar.SetValue(0)
     slide_bar.SetTitleText("Scalar Value")
     
+    # Set colors.
     slide_bar.GetSliderProperty().SetColor(red_r, red_g, red_b)
     slide_bar.GetTitleProperty().SetColor(white, white, white)
     slide_bar.GetLabelProperty().SetColor(red_r, red_g, red_b)
@@ -43,6 +98,7 @@ def generate_slide_bar():
     slide_bar.GetTubeProperty().SetColor(white, white, white)
     slide_bar.GetCapProperty().SetColor(red_r, red_g, red_b)
     
+    # Set coordinates.
     slide_bar.GetPoint1Coordinate().SetCoordinateSystemToNormalizedDisplay()
     slide_bar.GetPoint1Coordinate().SetValue(0.78, 0.1)
     
@@ -57,41 +113,7 @@ def custom_callback(obj, event):
     warp.SetScaleFactor(value)
     warp.Update()
 
-def main():
-    # Define stuff
-    vti_file, texture_file = get_program_parameters()
-    colors = vtk.vtkNamedColors() 
-    
-    # Read vti file
-    reader = vtk.vtkXMLImageDataReader()
-    reader.SetFileName(vti_file)
-    
-    # Load texture
-    readerFactory = vtk.vtkImageReader2Factory()
-    textureFile = readerFactory.CreateImageReader2(texture_file)
-    textureFile.SetFileName(texture_file)
-    textureFile.Update()
-
-    atext = vtk.vtkTexture()
-    atext.SetInputConnection(textureFile.GetOutputPort())
-    atext.InterpolateOn()
-
-    # generate warp from vti.
-    global warp
-    warp.SetInputConnection(reader.GetOutputPort())
-    warp.SetScaleFactor(-75)
-    
-    mapper = vtk.vtkDataSetMapper()
-    mapper.SetInputConnection(warp.GetOutputPort())
-    mapper.SetScalarRange(0, 255)
-    mapper.ScalarVisibilityOff()
-    
-    actor = vtk.vtkActor()
-    actor.SetMapper(mapper)
-    
-    # texture assigment doesn't work.
-    actor.SetTexture(atext)
-    
+def generate_gui(actor):
     # Create renderer stuff
     renderer = vtk.vtkRenderer()
     renderer_window = vtk.vtkRenderWindow()
@@ -99,7 +121,7 @@ def main():
     renderer_window_interactor = vtk.vtkRenderWindowInteractor()
     renderer_window_interactor.SetRenderWindow(renderer_window)
     
-    # add slide bar   
+    # Add slide bar   
     slide_bar = generate_slide_bar()
     slider_widget = vtk.vtkSliderWidget()
     slider_widget.SetInteractor(renderer_window_interactor)
@@ -108,7 +130,7 @@ def main():
     slider_widget.EnabledOn()
 
     
-    # Add the actors to the renderer, set the background and size
+    # Add the actor and camera to the renderer, set background and size
     renderer.AddActor(actor)
     renderer.ResetCamera()
     renderer.GetActiveCamera().Azimuth(180)
@@ -117,15 +139,36 @@ def main():
     renderer.GetActiveCamera().Elevation(0)
     renderer.SetBackground(0.1, 0.1, 0.1)
     renderer.ResetCameraClippingRange()
-    
-    renderer_window.SetSize(1200, 800)
-    
+    renderer_window.SetSize(renderer_window.GetScreenSize());
     cam1 = renderer.GetActiveCamera()
     cam1.Zoom(1.5)
     
     renderer_window_interactor.Initialize()
     renderer_window.Render()
+    renderer_window.SetWindowName('Heightmap Visualizer')
+    renderer_window.Render()
     renderer_window_interactor.Start()
+    
+
+def main():
+    # Get file paths from cli params.
+    data_file, texture_file = get_program_parameters()
+    
+    # Read data file.
+    data = read_file(data_file)
+    if(data):
+        # Generate texture.
+        texture = generate_texture(texture_file)        
+        if(texture):      
+            # Generate actor.
+            actor = generate_actor(data, texture)
+            # Generate GUI
+            generate_gui(actor)
+        else:
+            print('The texture file was not found or the file provided does not match the .jpg extension.')
+    else:
+        print('The data file was not found or the file provided does not match neither the .vti and .vtp extension.')
+    
 
 if __name__ == '__main__':
     main()
